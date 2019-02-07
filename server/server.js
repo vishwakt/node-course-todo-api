5fa37afb7ec6427395fc069898ffb6920d5cdfa8
Vishwak <vishwakt@gmail.com>
2019-02-06 16:30:05 -0800

    Adding user to DB using mongoose

--- server/server.js
+++ server/server.js
@@ -3,22 +3,48 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/TodoApp');
 
-var Todo = mongoose.model('Todo', {
-    text: {
-        type: String
-    },
-    completed: {
-        type: Boolean
-    },
-    completedAt: {
-        type: Number
+// var Todo = mongoose.model('Todo', {
+//     text: {
+//         type: String,
+//         required: true,
+//         minlength: 1,
+//         trim: true
+//     },
+//     completed: {
+//         type: Boolean,
+//         default: false
+//     },
+//     completedAt: {
+//         type: Number,
+//         default: null
+//     }
+// });
+
+// var newTodo = new Todo({
+//     text: '   Edit this video   '
+// });
+//
+// newTodo.save().then((doc) => {
+//     console.log(JSON.stringify(doc, undefined, 2));
+// }, (err) => {
+//     console.log('Unable to save Todo', err)
+// });
+
+var User = mongoose.model('User', {
+    email: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
     }
 });
 
-var newTodo = new Todo();
+var user = new User({
+    email: 'vishwakt@gmail.com'
+});
 
-newTodo.save().then((doc) => {
-    console.log('Saved Todo', doc)
+user.save().then((doc) => {
+    console.log('User saved', doc)
 }, (err) => {
-    console.log('Unable to save Todo', err)
+    console.log('User expected', err)
 });
