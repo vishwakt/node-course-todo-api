e7a6c1c5ff1cc0e064dcacd10fedf82e27a1722b
Vishwak <vishwakt@gmail.com>
2019-02-06 19:46:49 -0800

    Added tests

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.post('/todos', (req, res) => {
     });
 
     todo.save().then((doc) => {
-        res.send(doc);
+        res.status(200).send(doc);
     }, (err) => {
         res.status(400).send(err)
     })
@@ -26,3 +26,7 @@ app.post('/todos', (req, res) => {
 app.listen(3000, () => {
     console.log('Started on port 3000')
 });
+
+module.exports = {
+    app
+};
